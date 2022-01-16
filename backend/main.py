719ef588994765f7fb71295914bb559b4bdf740c
jeswanthmukesh20 <jeswanthmukesh69@gmail.com>
2022-01-16 21:46:59 +0530

    updated project with reactja and axio for async data transfer

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -8,16 +9,30 @@ from PIL import Image
 
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+]
 
+# what is a middleware? 
+# software that acts as a bridge between an operating system or database and applications, especially on a network.
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/uploadfile/")
+@app.post("/file-upload/")
 async def create_upload_file(request: Request, files: UploadFile = File(...)):
     img = Image.open(files.file)
     preds = pytesseract.image_to_string(img)
-    print(preds)
-    return templates.TemplateResponse("response.html", {"request": request, "response": preds})
+    # print(preds)
+    return {"content": preds}
+    # return templates.TemplateResponse("response.html", {"request": request, "content": preds})
